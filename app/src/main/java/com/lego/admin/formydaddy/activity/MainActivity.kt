4e6bbf4ca71cc9f3968c9cb6728811aa0fad9988
d.kovalenko <d.kovalenko@pfsoft.org>
2017-05-29 19:26:56 +0300

    img + base classes

---
+++ app/src/main/java/com/lego/admin/formydaddy/activity/MainActivity.kt
@@ -0,0 +1,1 @@
+package com.lego.admin.formydaddy.activityimport android.support.v7.app.AppCompatActivityimport android.os.Bundleimport com.lego.admin.formydaddy.Rclass MainActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        setContentView(R.layout.activity_main)    }}
