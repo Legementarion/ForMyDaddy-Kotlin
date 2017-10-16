677e6d262ce9cdf4f0c2548e706a49c152619a6d
Legementarion <legementario357442829>
2017-10-16 19:31:56 +0300

    domino check + utils

---
+++ app/src/main/java/com/lego/admin/formydaddy/logic/Utils.kt
@@ -0,0 +1,11 @@
+package com.lego.admin.formydaddy.logic
+
+class Utils {
+
+    companion object {
+        const val DECK_SIZE = 28
+        const val ZERO = "0"
+        const val DOMINO_STATS = "domino_stats"      //for shared preference
+        const val DOMINO_WINS = "domino_wins"
+    }
+}
