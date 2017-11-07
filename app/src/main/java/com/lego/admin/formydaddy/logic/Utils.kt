ae34eaf4bec4c6fb080a3125119e394b06b42297
Legementarion <legementario357442829>
2017-11-07 23:16:47 +0200

    di + events

--- app/src/main/java/com/lego/admin/formydaddy/logic/Utils.kt
+++ app/src/main/java/com/lego/admin/formydaddy/logic/Utils.kt
@@ -5,7 +5,14 @@ class Utils {
     companion object {
         const val DECK_SIZE = 28
         const val ZERO = "0"
-        const val DOMINO_STATS = "domino_stats"      //for shared preference
+
+        //for shared preference
+        const val DOMINO_STATS = "domino_stats"
         const val DOMINO_WINS = "domino_wins"
+
+        //for Firebase event
+        const val EVENT_END_OF_GAME = "event_game"
+        const val EVENT_WINS = "event_win"
+        const val EVENT_GAMES = "event_games"
     }
 }
